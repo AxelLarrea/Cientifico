d7352d0588ff0a0bd7efe3293703b730d140d2c9
Axel Larrea <63266736+AxelLarrea@users.noreply.github.com>
2021-08-28 21:23:07 -0300

    28-08-2021 "Webpack funcionando"

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,3 @@
-// const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const CopyWebpackPlugin = require('copy-webpack-plugin');
-
-
-// module.exports = {
-//     entry: '.src/index.js', //Punto de entrada con su direccion
-//     output: {
-//         path: path.resolve(__dirname, 'dist'),
-//         filename: 'main.js' //Este es el nombre del archivo final para produccion
-//     }
-// };
-
-// resolve: {
-//     extensions: ['.js']
-// };
-
-// module: {
-//     rules: [//reglas de webpack
-//         {
-//             test: /\.js$/,
-//             exclude : /node_modules/,
-//             use: {
-//                 loader: 'babel-loader'
-//             }
-//         }
-//     ]
-// };
-
-// plugins: [ //Plugins de webpack
-//     new HtmlWebpackPlugin(
-//         {
-//             inject: true, //Como inyectar un valor en html
-//             template: './public/index.html',
-//             filename: './index.html'
-//         }
-//     ),
-//     new CopyWebpackPlugin([{
-//         from: './src/styles/styles.css',
-//         to: ''
-//     }])
-// ]
-
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
@@ -50,19 +7,19 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
-},
+    },
 resolve: {
-    extensions: ['.js'],
+    extensions: ['.js']
 },
 module: {
     rules: [
         {
             test: /\.js?$/,
             exclude: /node_modules/,
-    use: {
-        loader: 'babel-loader',
-    }
-    }
+            use: {
+                loader: 'babel-loader',
+            }
+        }
     ]
 },
 plugins: [
@@ -73,9 +30,13 @@ plugins: [
             filename: './index.html',
         }
     ),
-    new CopyWebpackPlugin([{
-    from: './src/styles/styles.css',
-    to: ''
-    }])
+    new CopyWebpackPlugin({
+        patterns: [
+            {
+                from: './src/styles/styles.css',
+                to: ''
+            }
+        ]
+    })
     ]
 }
